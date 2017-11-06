c0d01e742bfa3b5dbb5089a996355669f223fc0f
Alexander Zaitsev <zamazan4ik@tut.by>
2017-11-06 04:00:12 +0300

    Fix a lot stuff

--- conanfile.py
+++ conanfile.py
@@ -1,27 +1,31 @@
-from conans import ConanFile
+from conans import ConanFile, tools
 
 
 class spdlogConan(ConanFile):
     name = "spdlog"
     version = "0.14.0"
     license = "MIT"
-    url = "https://github.com/memsharded/conan-spdlog"
+    url = "https://github.com/bincrafters/conan-spdlog"
     options = {"fmt_external": [True, False]}
     default_options = "fmt_external=False"
 
     def requirements(self):
         if self.options.fmt_external:
-            self.requires("fmt/3.0.1@memsharded/stable")
+            self.requires("fmtlib/4.0.0@bincrafters/stable")
 
     def source(self):
-        self.run("git clone https://github.com/gabime/spdlog.git")
-        self.run("cd spdlog && git checkout v%s" % self.version)
+        base_url = "https://github.com/gabime/spdlog/archive"
+        zip_name = "v%s.zip" % self.version
+        tools.download("%s/%s" % (base_url, zip_name), zip_name)
+        tools.unzip(zip_name)
+        #self.run("git clone https://github.com/gabime/spdlog.git")
+        #self.run("cd spdlog && git checkout v%s" % self.version)
 
     def package(self):
-        self.copy("*.h", dst="include", src="spdlog/include")
-        self.copy("*ostream.cc", dst="include", src="spdlog/include")
+        self.copy("*.h", dst="include", src="spdlog-%s/include" % (self.version))
+        self.copy("*ostream.cc", dst="include", src="spdlog-%s/include" % (self.version))
         if not self.options.fmt_external:
-            self.copy("*format.cc", dst="include", src="spdlog/include")
+            self.copy("*format.cc", dst="include", src="spdlog-%s/include" % (self.version))
 
     def package_info(self):
         if self.options.fmt_external:
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "spdlog/spdlog.h"
+#include <spdlog/spdlog.h>
 
 int main(int, char* [])
 {
