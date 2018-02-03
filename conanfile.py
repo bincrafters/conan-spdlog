15c335b805f9ad47fcaf4b5646c9ce56a15f3632
Alexandre Petitjean <alpetitjean@gmail.com>
2018-02-03 21:37:06 +0100

    Back to 0.14.0, fix pthread link

--- conanfile.py
+++ conanfile.py
@@ -6,9 +6,10 @@ from conans import ConanFile, tools
 
 class spdlogConan(ConanFile):
     name = "spdlog"
-    version = "0.16.3"
+    version = "0.14.0"
     license = "MIT"
     exports = ["LICENSE.md"]
+    settings = "os"
     description = "Very fast, header only, C++ logging library."
     url = "https://github.com/bincrafters/conan-spdlog"
     options = {"fmt_external": [True, False]}
@@ -33,6 +34,8 @@ class spdlogConan(ConanFile):
     def package_info(self):
         if self.options.fmt_external:
             self.cpp_info.defines.append("SPDLOG_FMT_EXTERNAL")
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
 
     def package_id(self):
         self.info.header_only()
