4bb8f8325e9cb5e0d533eed1db4dd4c500fd3b2a
Croydon <git@cr0ydon.com>
2019-10-19 21:38:09 +0200

    Provide prebuilds for lib as well

--- build.py
+++ build.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-from bincrafters import build_template_header_only
+from bincrafters import build_template_default, build_template_header_only
+import os
 
 if __name__ == "__main__":
+    header_only = os.getenv("CONAN_OPTIONS", False)
 
-    builder = build_template_header_only.get_builder()
+    builder = build_template_default.get_builder(pure_c=False)
+
+    if header_only:
+        builder = build_template_header_only.get_builder()
 
     builder.run()
